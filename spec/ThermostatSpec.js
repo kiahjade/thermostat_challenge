a7a65c1b00ad2544d16499efbef03397a4633d7a
Kiah Hewitt <hewittkiah@gmail.com>
2019-05-02 16:09:05 +0100

    added minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe ('thermostat', function() {
   var thermostat;
 
@@ -9,8 +11,18 @@ describe ('thermostat', function() {
     expect(thermostat.currentTemperature()).toEqual(20);
   });
 
-  it ('increases temperature', function(degree) {
+  it ('allows temperature to be increased', function() {
     thermostat.up(10);
     expect(thermostat.currentTemperature()).toEqual(30);
+  });
+
+  it ('allows temperature to be decreased', function() {
+    thermostat.down(5);
+    expect(thermostat.currentTemperature()).toEqual(15);
+  });
+
+  it ('has a minimum temp of 10 degrees', function() {
+    thermostat.down(15);
+    expect(thermostat.currentTemperature()).toEqual(10);
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,14 +1,19 @@
+'use strict';
+
 function Thermostat() {
   this.temperature = 20;
+  this.MIN_TEMP = 10;
 }
 
 Thermostat.prototype.currentTemperature = function() {
   return this.temperature;
 };
 
-Thermostat.prototype.up = function(degree) {
-  this.temperature += degree
+Thermostat.prototype.up = function(num) {
+  return this.temperature += num;
 };
 
-
-// console.log(Thermostat.prototype.up(5));
+Thermostat.prototype.down = function(num) {
+  if ((this.temperature - num) >= this.MIN_TEMP) {return this.temperature -= num}
+  else { return this.temperature = 10;}
+};
